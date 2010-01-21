60eb74b50a9085468ef8e749ee328e3bd8a30266
Hedgehog <hedgehogshiatus@gmail.com>
2010-01-21 16:45:22 +1100

    extend nanoc3 website helpers and configuration.

--- website/lib/default.rb
+++ website/lib/default.rb
@@ -1,2 +1,19 @@
-# All files in the 'lib' directory will be loaded
-# before nanoc starts compiling.
+require 'rubygems'
+require 'extlib'
+require 'pathname'
+require 'nanoc3'
+
+dir = (Pathname(__FILE__).dirname / '..' / 'lib' / 'scar').expand_path
+require dir
+
+include Nanoc3::Helpers::Blogging
+include Nanoc3::Helpers::Breadcrumbs
+include Nanoc3::Helpers::Capturing
+include Nanoc3::Helpers::Filtering
+include Nanoc3::Helpers::HTMLEscape
+include Nanoc3::Helpers::LinkTo
+include Nanoc3::Helpers::Rendering
+include Nanoc3::Helpers::Tagging
+include Nanoc3::Helpers::Text
+include Nanoc3::Helpers::XMLSitemap
+# include Scar::Helpers  # Classes and methods avilable during nonoc3's compilation.s

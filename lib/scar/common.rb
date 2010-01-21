7c901cb4632a86c07089ac80707b279ce0f09e51
Hedgehog <hedgehogshiatus@gmail.com>
2010-01-21 17:35:28 +1100

    initial scar library commit

---
+++ lib/scar/common.rb
@@ -0,0 +1,66 @@
+
+    ##
+    # Create a report
+    #
+    # @version
+    #
+    # @since
+    #
+    # Link to other code objects or files. The syntax is {ObjectName#method OPTIONAL_TITLE}
+    #
+    #
+    # @param name
+    # @param [Symbol] type
+    # @param [Array<String>] accounts a list of accounts to include
+    # @param [Optional Number] num_rows the number of rows
+    #
+    # @return
+    # @overload: Describe that your method can be used in various contexts with
+    #   various parameters or return types. The first line should declare the
+    #   new method signature, and the following indented tag data will be a new
+    #   documentation string with its own tags adding metadata for such an
+    #   overload.
+    #
+    # @overload set(value)
+    #   Sets a value on the default key `:foo`
+    #   @param [Object] value describe value param
+    #
+    # @option
+    #
+    # @example
+    #
+    # @see
+    #
+    # @author
+    # 
+    #
+
+# Header tables
+# HTTP Status Codes
+# Messages/Version Data Model
+
+# module Common Behaviors
+#   classes
+#     Transport Protocol
+#     URI Space
+#     Media Types
+#     Request Headers
+#     Request Parameters
+#     Response Headers
+#     HTTP Status Codes
+#     Error Response Message Bodies
+#       Messages Data Model
+#       Message Data Model
+#     Version Request Type
+#       Version Data Model
+#
+#
+#
+
+module Scar
+  module Helpers
+
+  end
+end
+
+

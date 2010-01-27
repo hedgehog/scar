09cf20fcd4cddaa5a10e0f528bfadf91a4abd126
Hedgehog <hedgehogshiatus@gmail.com>
2010-01-27 11:13:02 +1100

    fixup migrate script

--- gh-pages-migrate.sh
+++ gh-pages-migrate.sh
@@ -2,10 +2,10 @@
 pushd ./gh-pages
 rm -rf *
 cp --recursive ./../website/output/* .
+git branch -a
+git remote -v
 git checkout --track -b gh-pages origin/gh-pages
 git add .
 git commit -a -m "Migrate nanoc3 co output to gh-pages $1"
-git branch -a
-git remote -v
 # git push --force origin gh-pages
 popd
